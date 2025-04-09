35c85dc662442ff1a78965e2f71af1e1d61acc4a
Ruslan Yunisov <runisov@gmail.com>
2025-04-09 21:45:50 +0300

    Added a LeetCode #1768

---
+++ src/code-challenge/1768-Merge-Strings-Alternately/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+LeetCode: 1768
+Url: https://leetcode.com/problems/merge-strings-alternately/
+"""
+
+
+import unittest
+
+
+class Solution:
+    def main(self, s1, s2):
+        res = ""
+        i = 0
+        maxx = max(len(s1), len(s2))
+
+        while i < maxx:
+            if i < len(s1) and i < len(s2):
+                print(f"DEBUG01: {i} {s1}, {s2}, {res}")
+                res += s1[i]
+                res += s2[i]
+
+            if i < len(s1) and i > len(s2):
+                print(f"DEBUG02: {i} {s1}, {s2}, {res}")
+                res += s1[i-1::]
+
+            if i < len(s2) and i > len(s1):
+                print(f"DEBUG03: {i} {s1}, {s2}, {res}")
+                res += s2[i-1::]
+
+            i += 1
+
+        return res
+
+
+class TestSolution(unittest.TestCase):
+    def test_main(self):
+        s = Solution()
+        # self.assertEqual(s.main("abc", "pqr"), "apbqcr")
+        # self.assertEqual(s.main("ab", "pqrs"), "apbqrs")
+        self.assertEqual(s.main("abcd", "pq"), "apbqcd")
+
+
+if __name__ == "__main__":
+    unittest.main()
